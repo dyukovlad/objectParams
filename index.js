ec0c3325efe8addbdc8cda4f6e3ca7fef409bd30
Dyukov Vlad <dyukovlad@gmail.com>
2020-02-19 09:55:57 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+function FormParamsObject(objectParams){
+    
+    this.objectParams = objectParams;
+
+    this.getFullObject = function() {
+        return this.objectParams || null;
+    };
+
+    this.convertObjectToArray = function(propPath) {
+
+        if (!propPath.trim()) return;
+        
+        var newArr = [];
+        var obj = this.getObjectProperty(propPath);
+		
+        for (key in obj) {
+          var ln = obj[key];
+          for (var i = 0; i < ln.length; i++) {
+            if (!arr[i]) {
+              arr[i] = {};
+            } 
+            arr[i][key] = ln[i];
+          }
+        }
+        return this.setObjectProperty(propPath, arr);
+    };
+
+    this.getObjectProperty = function(propPath){
+
+        if (!propPath.trim()) return;
+        
+        var obj = this.objectParams;
+        var path = propPath.split('.');
+
+        if (path.length > 0){
+            
+            var ln = path.length;
+            for (var i = 0; i < ln; i++) {
+                if (obj === undefined) break;
+                obj = obj[path[i]];
+            }
+        }
+
+        return obj !== undefined ? obj : null;
+    };
+
+    this.setObjectProperty = function(propPath, value){
+
+        if (!propPath.trim() || !value.trim()) return;
+
+        var obj = this.objectParams;
+        var path = propPath.split('.');
+
+        if (path.length > 0){
+
+            var ln = path.length;
+            for (var i = 0; i < ln - 1; i++) {
+                
+                var elem = path[i];
+                if (!obj[elem]) {
+                    obj[elem] = {};
+                } else {
+                    if (typeof(obj[elem]) != "object") {
+                        obj[elem] = {};
+                    }
+                }
+                obj = obj[elem];
+            }
+        
+            obj[path[ln - 1]] = value;
+    
+            return obj[path[ln - 1]];
+        }
+        
+    };
+    
+}
